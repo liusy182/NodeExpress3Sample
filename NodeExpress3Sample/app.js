8d245d0cfe78fe34b5dc2d9825a5d89d813d123a
liusy182 <liusy182@hotmail.com>
2015-04-10 16:08:58 +0800

    added view for photo

--- NodeExpress3Sample/app.js
+++ NodeExpress3Sample/app.js
@@ -8,13 +8,15 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var photos = require('./routes/photos');
 
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
+//app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -24,13 +26,24 @@ app.use(app.router);
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
+//customized setings
+app.use('photos', __dirname + 'public/photos');
+
 // development only
 if ('development' == app.get('env')) {
     app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+//app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/', photos.list);
+
+//app.get('/', function () {
+//    res.render('index');
+//});
+//app.get('/feed', function () { 
+//    res.render()
+//});
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
---
+++ NodeExpress3Sample/routes/photos.js
@@ -0,0 +1,20 @@
+﻿'use strict';
+var photos = [];
+
+photos.push({
+    name: 'sample png 1',
+    path: 'http://www.fnordware.com/superpng/pnggrad16rgb.png'
+});
+
+photos.push({
+    name: 'sample png 2',
+    path: 'http://www.fnordware.com/superpng/pngtest8rgba.png'
+});
+
+
+exports.list = function (req, res) {
+    res.render('photos', {
+        title: 'photos',
+        photos: photos
+    });
+}
